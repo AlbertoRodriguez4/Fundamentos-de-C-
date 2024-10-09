3e1bedd258773a8f0a0a2610ec05e57bb4b5ff3d
Alberto <Albertorope04@gmail.com>
2024-10-09 18:33:09 +0200

    Ejercicio 4 completado

--- obj/Debug/net8.0/dia 1.AssemblyInfo.cs
+++ obj/Debug/net8.0/dia 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dia 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391324569622cc2e57a97009a8c0f5a928da33e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+309371031661d312157b9af57f471dab35ef0285")]
 [assembly: System.Reflection.AssemblyProductAttribute("dia 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dia 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
